e7fb89df7f20b47f48e6189fffc9959841353418
Gustavo Alves Fonseca De Lima <guuhalves0922@gmail.com>
2025-07-16 22:03:22 -0300

    Feat: Edit Controllers/Hotfix bugs

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,22 @@ import fastify from "fastify";
 import { funcionarioRoutes } from "./interfaces/routes/funcionarioRoutes";
 import { entradaSaidaRoutes } from "./interfaces/routes/entradaSaidaRoutes";
 import { pausasRoutes } from "./interfaces/routes/pausasRoutes";
+import { FuncionarioEscalaRoutes } from "./interfaces/routes/funcionarioEscalaRoutes";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = fastify({ logger: true });
 
 app.register(funcionarioRoutes);
 app.register(entradaSaidaRoutes);
 app.register(pausasRoutes);
+app.register(FuncionarioEscalaRoutes);
 
-app.listen({ port: 3333 }, err => {
+app.listen({ port: Number(process.env.PORT) }, (err, address) => {
     if (err) {
-        app.log.error(err),
-        process.exit(1)
+        app.log.error(err);
+        process.exit(1);
     }
-})
+    app.log.info(`Servidor rodando em ${address}`);
+});
